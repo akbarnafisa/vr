5f3ef36febe64d9e80b40a8dc8a921e663864881
Akbar Nafisa <akbarnj788@gmail.com>
2020-06-18 13:53:10 +0700

    update styling

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,43 @@
-// vue.config.js
-const path = require('path')
+// // vue.config.js
+// const path = require('path')
 
+// module.exports = {
+//   chainWebpack: config => {
+//     const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+//     types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
+//   },
+// }
+
+// function addStyleResource (rule) {
+//   rule.use('style-resource')
+//     .loader('style-resources-loader')
+//     .options({
+//       patterns: [
+//         path.resolve(__dirname, './src/scss/settings.scss'),
+//       ],
+//     })
+// }
+
+
+const path = require('path');
 module.exports = {
-  chainWebpack: config => {
-    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
-    types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: []
+  }
+},
+chainWebpack: config => {
+  const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+  types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
   },
 }
-
 function addStyleResource (rule) {
   rule.use('style-resource')
     .loader('style-resources-loader')
     .options({
       patterns: [
-        path.resolve(__dirname, './src/scss/settings.scss'),
+        path.resolve(__dirname, './src/styles/variables/variables.scss'),
       ],
     })
 }
